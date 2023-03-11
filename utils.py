a648c87d72a727ac4abd089a97340f65446010b2
Abel Tavares <121238257+abeltavares@users.noreply.github.com>
2023-03-11 10:49:45 +0000

    add copyright

--- utils.py
+++ utils.py
@@ -20,7 +20,7 @@ def config_menu_footer() -> None:
                 visibility: hidden;
             }}
             footer:before {{
-            content:"Copyright © 2023 {author_name}; 
+            content:"Copyright © 2023 Abel Tavares; 
             visibility: visible;
             display: block;
             position: relative;
